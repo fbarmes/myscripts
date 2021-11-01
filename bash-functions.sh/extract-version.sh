9efa45404732fb97d475d10cf94777596aee44f8
fbarmes <f.barmes@free.fr>
2021-11-01 10:34:41 +0100

    add test function

--- bash-functions.sh/extract-version.sh
+++ bash-functions.sh/extract-version.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+#-------------------------------------------------------------------------------
+# COLORS
+#-------------------------------------------------------------------------------
+declare -r NC='\033[0m'
+declare -r BOLD='\033[1m'
+declare -r RED='\033[0;31m'
+declare -r GREEN='\033[0;32m'
+
+#-------------------------------------------------------------------------------
+function echo_green() {
+  echo -e "${GREEN}${BOLD}${@}${NC}"
+}
+
+#-------------------------------------------------------------------------------
+function echo_red() {
+  echo -e "${RED}${BOLD}${@}${NC}"
+}
+
 
 #-------------------------------------------------------------------------------
 function get_version() {
@@ -18,25 +36,34 @@ function get_version() {
 }
 
 #-------------------------------------------------------------------------------
+function test_get_version() {
+  input=${1}
+  expected="${2}"
+  result=$(get_version ${input})
+
+  echo "-- testing"
+  echo "  input=     [${input}]"
+  echo "  expected=  [${expected}]"
+  echo "  result=    [${result}]"
+
+  if [[ ${result} == ${expected} ]] ; then
+    echo_green "OK"
+  else
+    echo_red "FAILED"
+  fi
+  echo ""
+}
+
+#-------------------------------------------------------------------------------
 function main() {
 
   echo "Start main"
 
-  input="one-two-thr.ee-1.0.2-SNAPSHOT.tgz"
-  echo "-- ${input}"
-  version=$(get_version ${input})
-  echo "${version}"
-
-  input="one-two-thr.ee-1.0.2.tgz"
-  echo "-- ${input}"
-  version=$(get_version ${input})
-  echo "${version}"
-
-  input="one-two-three-1.0.2_HF05-SNAPSHOT.tgz"
-  echo "-- ${input}"
-  version=$(get_version ${input})
-  echo "${version}"
-
+  test_get_version "one_two-three-1.0.2.tgz" "1.0.2"
+  test_get_version "one-two-three-1.0.2.tgz" "1.0.2"
+  test_get_version "one-two-three-12.152.tgz" "12.152"
+  test_get_version "one-two-three-1.0.2-SNAPSHOT.tgz" "1.0.2-SNAPSHOT"
+  test_get_version "one_two-three-134.65.2.tgz" "134.65.2"
 
 }
 
